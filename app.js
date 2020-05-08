3e4824ea3c28189facbd2b5fe96ab54260771e6e
dogq2 <keith.j.mchugh@gmail.com>
2020-05-08 17:57:23 +0000

    change connection ports 2

--- app.js
+++ app.js
@@ -60,9 +60,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP function() {
-    console.log("Server up");
-});
+app.listen(3000);
 
 // app.listen(3000, function() {
 //     console.log("Server up");
