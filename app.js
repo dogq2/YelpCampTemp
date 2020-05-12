1b452e8977190e92ddbbf8e2b3028388a4ba9cec
dogq2 <keith.j.mchugh@gmail.com>
2020-05-12 16:40:29 +0000

    update app.listen 3

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ var port = process.env.port || 3000;
 //app.listen(port);
 
 
-app.listen(port, function(){
+app.listen(port,process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
 });
 
